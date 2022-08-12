f13015051c3bc619b368ba4902052dbc4428ee8b
RubensAmaralNeto <111020660+RubensAmaralNeto@users.noreply.github.com>
2022-08-12 19:47:59 -0300

    Add files via upload

--- 003-projeto/app.js
+++ 003-projeto/app.js
@@ -4,10 +4,10 @@ var aumento =(0.15);
 
 var inflacao =(0.07);
 
-var novoSalario =(salario + (salario * aumento));
+var novoSalario =salario + (salario * aumento);
 
-var novoSalarioInflacao = (novoSalario - (novoSalario * inflacao));
+var novoSalarioInflacao = novoSalario - (novoSalario * inflacao);
 
-console.log('novo salario =' novoSalario );
-console.log('novo salario com inflacao = ' novoSalarioInflacao)
+console.log('novo salario = ' novoSalario );
+console.log('novo salario com inflacao = ' novoSalarioInflacao);
 
