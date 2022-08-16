eae363f5c361088748eaf89953d960677099fcf1
RubensAmaralNeto <111020660+RubensAmaralNeto@users.noreply.github.com>
2022-08-15 21:00:16 -0300

    Add files via upload

---
+++ 004-projeto/app.js
@@ -0,0 +1,24 @@
+var num1=(5);
+var num2 =(2);
+var num3 ;
+var num5 = (12312.19998789);
+
+
+console.log('var num1 = ' + num1 );
+console.log('var num2 = ' + num2 );
+console.log('var not defined = ' + num3 + "\n");
+console.log( num5 + "\n" );
+
+var num3 = num1 / num2;
+var num4 = num1 % num2;
+
+console.log('var num3 = ' + num3 + "\n");
+
+ num3_round = Math.round(num3 + "\n");
+
+console.log('var resto = ' + num4 );
+console.log('var num3_round = ' +num3_round + "\n" );
+
+console.log('var num5 = ' + num5 );
+num5 = num5.toFixed(2);
+console.log('var num5 = ' + num5 );
