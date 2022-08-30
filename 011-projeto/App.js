1612d0ea659bf632612b1064fe381a118e53b837
RubensAmaralNeto <111020660+RubensAmaralNeto@users.noreply.github.com>
2022-08-30 20:35:46 -0300

    Add files via upload

--- 011-projeto/App.js
+++ 011-projeto/App.js
@@ -1,20 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {ActivityIndicator, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={[styles.container, styles.horizontal]}>
+        <ActivityIndicator />
+        <ActivityIndicator size="large" />
+        <ActivityIndicator size="small" color="red" />
+        <ActivityIndicator size="large" color="purple" />
+        <ActivityIndicator size="xxx-large" color="purple" />
+        <StatusBar style="auto" />
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center"
   },
+  horizontal: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 10,
+    borderWidth:5
+  }
 });
