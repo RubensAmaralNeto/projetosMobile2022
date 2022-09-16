f393bc9baeb178ea219b07ca2eb02441425d89b4
RubensAmaralNeto <111020660+RubensAmaralNeto@users.noreply.github.com>
2022-09-16 20:57:34 -0300

    Add files via upload

--- 015-projeto/App.js
+++ 015-projeto/App.js
@@ -4,27 +4,48 @@ import { StyleSheet, Text, View, Button, Modal, Pressable, Icon } from 'react-na
 import { Provider, Appbar, Avatar, TextInput } from 'react-native-paper';
 
 export default function App() {
-  const [firstName, setFirstNameText] = useState(null);
-  const [lastName, setLastNameText] = useState(null);
-  const [email, setEmailText] = useState(null);
-  const [password, setPasswordText] = useState(null);
+  const [firstName, setFirstNameText] = useState('');
+  const [lastName, setLastNameText] = useState('');
+  const [email, setEmailText] = useState('');
+  const [password, setPasswordText] = useState('');
+  const [msgErr, setmsgErrText] = useState('');
+
+  const validateAll = () => {
+    console.log("Validando forma");
+    
+    validateStrSize(firstName);
+    
+    validateStrSize(lastName);
+    validateStrSize(email);
+    validateStrSize(password);
+  }
+
+  const validateStrSize = (text) => 
+  {
+    if( text.length <= 3){
+      console.log("Campo preechido incorretamente.");
+      setmsgErrText("Algum campo preechido incorretamente.");
+      return false;
+    }
+  }
 
   return (
     <View style={styles.container}>
       <Text style={styles.labelText}> Nome: </Text>
-      <TextInput style={styles.inputText} label="informe o seu Nome" secureTextEntry={true} placeholder="Informe o seu nome" onChangeText={firstName => setFirstNameText(firstName)}  />
+      <TextInput style={styles.inputText} label="Nome" placeholder="Informe o seu nome" onChangeText={firstName => setFirstNameText(firstName)}  />
     
       <Text style={styles.labelText}> sobreNome: </Text>
-      <TextInput style={styles.inputText} label="informe o seu sobreNome" secureTextEntry={true} placeholder="Informe o seu sobreNome" onChangeText={lastName => setLastNameText(lastName)}  />
+      <TextInput style={styles.inputText} label="sobreNome" placeholder="Informe o seu sobreNome" onChangeText={lastName => setLastNameText(lastName)}  />
 
       <Text style={styles.labelText}> Email: </Text>
-      <TextInput style={styles.inputText} label="informe o e-mail" secureTextEntry={true} placeholder="Informe o seu e-mail" onChangeText={email => setEmailText(email)}  />
+      <TextInput style={styles.inputText} label="Email" placeholder="Informe o seu e-mail" onChangeText={email => setEmailText(email)}  />
 
       <Text style={styles.labelText}> Senha: </Text>
-      <TextInput style={styles.inputText} label="Senha" secureTextEntry={true} placeholder="Informe a sua senha" onChangeText={password => setLastNameText(password)}  />
+      <TextInput style={styles.inputText} label="Senha" secureTextEntry={true} placeholder="Informe a sua senha" onChangeText={password => setPasswordText(password)}  />
     
 
-      <Button title="Submit" style={styles.buttonstyle} color="6200EE" />
+      <Button title="Submit" onPress={() => validateAll()} style={styles.buttonstyles} color="#6200EE" />
+      <Text>{msgErr}</Text>
     </View>
   );
 }
@@ -36,15 +57,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  labelText:{
-    marginTop: 10,
-    marginBotton: 5,
+  labelText:{    
+    marginTop: 15,
+    marginBotton:5
   },
-  inputText:{
+  inputText:{    
     height: 45,
     marginBottom: 15
   },
-  buttonStyles:{
+  buttonstyles:{
     marginTop: 10
   },
 });
