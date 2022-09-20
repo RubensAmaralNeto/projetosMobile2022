ddd462f071ade16fc4adae6244ac45d979bc8072
RubensAmaralNeto <111020660+RubensAmaralNeto@users.noreply.github.com>
2022-09-20 18:18:23 -0300

    Add files via upload

--- 015-projeto/App.js
+++ 015-projeto/App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useEffect, useState, component } from "react";
 import { StyleSheet, Text, View, Button, Modal, Pressable, Icon } from 'react-native';
-import { Provider, Appbar, Avatar, TextInput } from 'react-native-paper';
+import { Provider, Appbar, Avatar, TextInput, HelperText } from 'react-native-paper';
 
 export default function App() {
   const [firstName, setFirstNameText] = useState('');
@@ -9,6 +9,7 @@ export default function App() {
   const [email, setEmailText] = useState('');
   const [password, setPasswordText] = useState('');
   const [msgErr, setmsgErrText] = useState('');
+  const [secureTextEntry, setSecureTextEntry] = useState(true);
 
   const validateAll = () => {
     console.log("Validando forma");
@@ -28,21 +29,40 @@ export default function App() {
       return false;
     }
   }
+    const validateMail = () => {
+      const reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
+      //console.log(text, reg.test(text));
+      return !reg.test(email);
+    }; 
+  
 
   return (
     <View style={styles.container}>
+
+     {/*inicio campo nome */}
       <Text style={styles.labelText}> Nome: </Text>
       <TextInput style={styles.inputText} label="Nome" placeholder="Informe o seu nome" onChangeText={firstName => setFirstNameText(firstName)}  />
-    
+     {/* fim campo nome */}
+
+      {/*inicio campo sobrenome */}
       <Text style={styles.labelText}> sobreNome: </Text>
       <TextInput style={styles.inputText} label="sobreNome" placeholder="Informe o seu sobreNome" onChangeText={lastName => setLastNameText(lastName)}  />
+      {/*fim campo sobrenome */}
 
+      {/*inicio campo email */}
       <Text style={styles.labelText}> Email: </Text>
-      <TextInput style={styles.inputText} label="Email" placeholder="Informe o seu e-mail" onChangeText={email => setEmailText(email)}  />
+      <TextInput style={styles.inputText} label="Email" placeholder="Informe o seu e-mail" onChangeText={email => setEmailText(email)} defaultValue={email} />
 
+      <HelperText type="error" visible={validateMail()}>
+         O campo e-mail está invalido!
+      </HelperText>
+
+      {/*fim campo email */}
+
+      {/*inicio campo senha */}
       <Text style={styles.labelText}> Senha: </Text>
-      <TextInput style={styles.inputText} label="Senha" secureTextEntry={true} placeholder="Informe a sua senha" onChangeText={password => setPasswordText(password)}  />
-    
+      <TextInput style={styles.inputText} label="Senha" secureTextEntry={secureTextEntry} placeholder="Informe a sua senha" onChangeText={password => setPasswordText(password)} right = { <TextInput.Icon name = "eye"  onPress={() => {setSecureTextEntry(!secureTextEntry);return false;}} />} />   
+      {/*fim campo senha */}
 
       <Button title="Submit" onPress={() => validateAll()} style={styles.buttonstyles} color="#6200EE" />
       <Text>{msgErr}</Text>
